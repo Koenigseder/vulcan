58d3ac244ea15b5a58518c5e18c2cdef93531ca3
Kevin <kevin.muehlbauer@outlook.de>
2021-11-12 15:47:05 +0100

    feat: change footer layout at learnpage

--- package/frontend/components/Footer.tsx
+++ package/frontend/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Center, HStack, Icon, Pressable, Text } from "native-base";
-import { Ionicons, Feather } from "@expo/vector-icons";
+import { Ionicons, Feather, FontAwesome5 } from "@expo/vector-icons";
 import React from "react";
 
 interface FooterProps {
@@ -10,76 +10,103 @@ interface FooterProps {
 export const Footer = (props: FooterProps) => {
   return (
     <HStack bg="#ae4951" alignItems="center" shadow={6}>
-      <Pressable
-        opacity={props.selectedElement === 0 ? 1 : 0.5}
-        py="3"
-        flex={1}
-        onPress={() => props.setSelectedElement(0)}
-      >
-        <Center>
-          <Icon
-            mb="1"
-            as={
-              <Feather
-                name={props.selectedElement === 0 ? "book-open" : "book"}
-              />
-            }
-            color="white"
-            size="sm"
-          />
-          <Text color="white" fontSize="12">
-            Vokabeln
-          </Text>
-        </Center>
-      </Pressable>
-      <Pressable
-        opacity={props.selectedElement === 1 ? 1 : 0.5}
-        py="2"
-        flex={1}
-        onPress={() => props.setSelectedElement(1)}
-      >
-        <Center>
-          <Icon
-            mb="1"
-            as={
-              <Ionicons
-                name={
-                  props.selectedElement === 1 ? "md-home" : "md-home-outline"
+      {props.selectedElement !== 3 ? (
+        <>
+          <Pressable
+            opacity={props.selectedElement === 0 ? 1 : 0.5}
+            py="3"
+            flex={1}
+            onPress={() => props.setSelectedElement(0)}
+          >
+            <Center>
+              <Icon
+                mb="1"
+                as={
+                  <Feather
+                    name={props.selectedElement === 0 ? "book-open" : "book"}
+                  />
                 }
+                color="white"
+                size="sm"
               />
-            }
-            color="white"
-            size="sm"
-          />
-          <Text color="white" fontSize="12">
-            Home
-          </Text>
-        </Center>
-      </Pressable>
-      <Pressable
-        opacity={props.selectedElement === 2 ? 1 : 0.6}
-        py="2"
-        flex={1}
-        onPress={() => props.setSelectedElement(2)}
-      >
-        <Center>
-          <Icon
-            mb={1}
-            as={
-              <Ionicons
-                name={
-                  props.selectedElement === 2 ? "settings" : "settings-outline"
+              <Text color="white" fontSize="12">
+                Vokabeln
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable
+            opacity={props.selectedElement === 1 ? 1 : 0.5}
+            py="2"
+            flex={1}
+            onPress={() => props.setSelectedElement(1)}
+          >
+            <Center>
+              <Icon
+                mb="1"
+                as={
+                  <Ionicons
+                    name={
+                      props.selectedElement === 1
+                        ? "md-home"
+                        : "md-home-outline"
+                    }
+                  />
                 }
+                color="white"
+                size="sm"
               />
-            }
-            color="white"
-            size="sm"
-          />
-          <Text color="white" fontSize={12}>
-            Einstellungen
-          </Text>
-        </Center>
-      </Pressable>
+              <Text color="white" fontSize="12">
+                Home
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable
+            opacity={props.selectedElement === 2 ? 1 : 0.6}
+            py="2"
+            flex={1}
+            onPress={() => props.setSelectedElement(2)}
+          >
+            <Center>
+              <Icon
+                mb={1}
+                as={
+                  <Ionicons
+                    name={
+                      props.selectedElement === 2
+                        ? "settings"
+                        : "settings-outline"
+                    }
+                  />
+                }
+                color="white"
+                size="sm"
+              />
+              <Text color="white" fontSize={12}>
+                Einstellungen
+              </Text>
+            </Center>
+          </Pressable>
+        </>
+      ) : (
+        <Pressable
+          opacity={props.selectedElement === 3 ? 1 : 0.6}
+          py="2"
+          flex={1}
+          onPress={() => props.setSelectedElement(1)}
+        >
+          <Center>
+            <Icon
+              mb={1}
+              as={<FontAwesome5 name={"running"} />}
+              color="white"
+              size="sm"
+            />
+            <Text color="white" fontSize={12}>
+              Verlassen
+            </Text>
+          </Center>
+        </Pressable>
+      )}
     </HStack>
   );
 };
